5c9e8221fee3db6fe51d6bc6dd360b679c782463
alegq <yalovik678@gmail.com>
2024-05-31 14:42:48 +0300

    4

---
+++ components/block5/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import boxGreen from "@/public/assets/icons/block4/box-green.png";
+import disk from "@/public/assets/icons/block4/disk-green.png";
+import pyramid from "@/public/assets/icons/block4/pyramid-green.png";
+import subtract from "@/public/assets/icons/block4/subtract.svg";
+import star from "@/public/assets/icons/block4/star.svg";
+
+import Image from "next/image";
+
+import styles from "./styles.module.scss";
+
+const Block5: FC = () => {
+  return (
+    <div className={styles.containerBlock5}>
+      <div></div>
+    </div>
+  );
+};
+
+export default Block5;
